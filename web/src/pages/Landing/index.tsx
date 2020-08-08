ea26c8e901b50d11a20f1ff41e9092b3fb84d5e3
Maria Carolina <carol-martins7@hotmail.com>
2020-08-08 00:32:57 -0300

    finalização módulo frontend + backend

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
-
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
-import './styles.css';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+import './styles.css';
 
 function Landing(){
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  // useEffect(() => {}, []); função, quando eu quero disparar essa função?
+  useEffect(() => {
+    api.get('connections').then(res => {
+      console.log(res.data.total);
+      const {total} = res.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <>
       <div id="page-landing">
@@ -33,7 +47,7 @@ function Landing(){
             </Link>
           </div>
           <span className="total-connections">
-            Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+            Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
           </span>
         </div>
       </div>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,35 +1,92 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
+
+import { Search } from 'react-feather';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, {Teacher} from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import api from '../../services/api';
+
 import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
+
 
 export default function TeacherList(){
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  // useEffect(() => {}, []); função, quando eu quero disparar essa função?
+
+  async function searchTeachers(e: FormEvent) {
+    e.preventDefault();
+
+    const res = await api.get('classes', {
+      //query parms =>  parms
+      params: {
+        subject,
+        week_day,
+        time,
+      }
+    });
+
+    setTeachers(res.data);
+  }
+
   return (
     <div id="page-teacher-list" className="container">
      <PageHeader title="Estes são os proffys disponíveis">
-       <form action="" id="search-teachers">
-         <div className="input-block">
-           <label htmlFor="subject">Matéria</label>
-           <input type="text" id="subject"/>
-         </div>
+       <form id="search-teachers" onSubmit={searchTeachers}>
 
-         <div className="input-block">
-           <label htmlFor="week_day">Dia da semana</label>
-           <input type="text" id="week_day"/>
-         </div>
+       <Select
+          name="subject"
+          label="Matéria"
+          value={subject}
+          onChange={e => {setSubject(e.target.value)}}
+          options={[
+            { value: 'Programação Orientada a Objeto', label: 'Programação Orientada a Objeto' },
+            { value: 'Programação Estruturada', label: 'Programação Estruturada' },
+            { value: 'Estrutura de Dados', label: 'Estrutura de Dados' },
+            { value: 'Programação pra Internet', label: 'Programação pra Internet' },
+            { value: 'Projeto Orientado', label: 'Projeto Orientado' }
+          ]}
+        />
+        <Select
+          name="week_day"
+          label="Dia da semana"
+          value={week_day}
+          onChange={e => {setWeekDay(e.target.value)}}
+          options={[
+            { value: '0', label: 'Domingo' },
+            { value: '1', label: 'Segunda-feira' },
+            { value: '2', label: 'Terça-feira' },
+            { value: '3', label: 'Quarta-feira' },
+            { value: '4', label: 'Quinta-feira' },
+            { value: '5', label: 'Sexta-feira' },
+            { value: '6', label: 'Sábado' }
+          ]}
+        />
+        <Input
+          name="time"
+          label="Hora" 
+          type="time"
+          value={time}
+          onChange={e => {setTime(e.target.value) }}
+          />
 
-         <div className="input-block">
-           <label htmlFor="time">Hora</label>
-           <input type="text" id="time"/>
-         </div>
+          <button type="submit">
+            <Search />
+          </button>
        </form>
      </PageHeader>
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher}/>;
+        })}
+
       </main>
 
     </div>
